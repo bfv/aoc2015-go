0d6ead40cf0fad130b2b3b7b773c1d7090afbe04
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2021-11-26 10:12:17 +0100

    day03 b

--- day03/day03.go
+++ day03/day03.go
@@ -5,23 +5,25 @@ import (
 	"io/ioutil"
 )
 
-var grid map[string]int
+var gridA map[string]int
+var gridB map[string]int
 
 func main() {
-	a, b := solve("input.txt")
+	a := solveA("input.txt")
+	b := solveB("input.txt")
 	fmt.Printf("day 02, a: %v, b: %v", a, b)
 }
 
-func solve(filename string) (int, int) {
-	var a, b int
+func solveA(filename string) int {
+	var a int
 	var x, y int
 
-	grid = make(map[string]int)
+	gridA = make(map[string]int)
 
 	buf, _ := ioutil.ReadFile(filename)
 	s := string(buf)
 
-	bringPresent(x, y)
+	bringPresent(x, y, "a")
 	for _, r := range s {
 		switch r {
 		case '^':
@@ -33,20 +35,72 @@ func solve(filename string) (int, int) {
 		case '<':
 			x--
 		}
-		bringPresent(x, y)
+		bringPresent(x, y, "a")
 	}
 
 	// get answer a
-	for _, v := range grid {
+	for _, v := range gridA {
 		if v > 0 {
 			a++
 		}
 	}
 
-	return a, b
+	return a
 }
 
-func bringPresent(x int, y int) {
+func solveB(filename string) int {
+	var b int
+	var dx, dy int
+	var xs, ys int // santa
+	var xr, yr int // robo
+
+	gridB = make(map[string]int)
+	buf, _ := ioutil.ReadFile(filename)
+	s := string(buf)
+
+	bringPresent(0, 0, "b")
+	for i, r := range s {
+		dx = 0
+		dy = 0
+		switch r {
+		case '^':
+			dy = 1
+		case '>':
+			dx = 1
+		case 'v':
+			dy = -1
+		case '<':
+			dx = -1
+		}
+		if i%2 == 0 {
+			// santa's turn
+			xs += dx
+			ys += dy
+			bringPresent(xs, ys, "b")
+		} else {
+			// robo's turn
+			xr += dx
+			yr += dy
+			bringPresent(xr, yr, "b")
+		}
+	}
+
+	// get answer a
+	for _, v := range gridB {
+		if v > 0 {
+			b++
+		}
+	}
+	return b
+}
+
+func bringPresent(x int, y int, grid string) {
 	co := fmt.Sprintf("%v,%v", x, y)
-	grid[co]++
+	if grid == "a" {
+		gridA[co]++
+	} else if grid == "b" {
+		gridB[co]++
+	} else {
+		panic("wrong grid: " + grid)
+	}
 }
