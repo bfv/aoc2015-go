a239f516d30de2b24fa124074b12697bfde3579f
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2021-12-05 18:07:24 +0100

    day07 eod2

--- day07/day07.go
+++ day07/day07.go
@@ -7,13 +7,20 @@ import (
 	"strings"
 )
 
-type instruction struct {
-	value   int
-	inputA  string
-	inputB  string
-	operand string
-	target  string
+type wire struct {
+	name string
 }
+
+type instruction struct {
+	wireA   wire
+	wireB   wire
+	operand string
+	unary   bool
+	wireOut wire
+	done    bool
+}
+
+var register map[string]uint16
 
 type program []instruction
 
@@ -24,61 +31,110 @@ var booklet program
 func main() {
 
 	//gates = map[string]int{}
-	booklet := program{}
+	booklet = program{}
+	register = map[string]uint16{}
 
-	buf, _ := ioutil.ReadFile("input.txt")
+	buf, _ := ioutil.ReadFile("_input.txt")
 	lines := strings.Split(string(buf), "\r\n")
 
 	processInput(lines)
 
 	solveA("a")
 
-	fmt.Println()
-	for _, instr := range booklet {
-		if instr.operand == "assign" {
-			fmt.Printf("assign %v -> %v\n", instr.value, instr.target)
+	fmt.Println(booklet)
+}
+
+func solveA(letter string) uint16 {
+	a := iterateIntructions()
+	displayRegister()
+	return a
+}
+
+func iterateIntructions() uint16 {
+	var a uint16
+	found := false
+	done := false
+	for !found && !done {
+		displayRegister()
+		done = true
+		for i, instr := range booklet {
+			if !instr.done {
+				booklet[i] = execute(instr)
+				done = false
+			}
 		}
+		a, found = register["a"]
 	}
+	return a
 }
 
-func solveA(letter string) int {
-	return findA(letter)
+func execute(instr instruction) instruction {
+
+	if !hasValue(instr.wireA) || (!instr.unary && !hasValue(instr.wireB)) {
+		return instr
+	}
+
+	var v uint16
+	switch instr.operand {
+	case "AND":
+		v = register[instr.wireA.name] & register[instr.wireB.name]
+	case "OR":
+		v = register[instr.wireA.name] | register[instr.wireB.name]
+	case "NOT":
+		v = ^register[instr.wireA.name]
+	case "RSHIFT":
+		v = register[instr.wireA.name] >> register[instr.wireB.name]
+	case "LSHIFT":
+		v = register[instr.wireA.name] << register[instr.wireB.name]
+	}
+	instr.done = true
+	register[instr.wireOut.name] = v
+	return instr
 }
 
-func findA(letter string) int {
-
+func hasValue(w wire) bool {
+	_, valueFound := register[w.name]
+	return valueFound
 }
 
 func processInput(lines []string) {
 
 	for _, s := range lines {
-		// fmt.Println(s)
+
 		parts := strings.Split(s, " ")
-		instr := instruction{value: -1}
+
 		if v, err := strconv.Atoi(parts[0]); err == nil {
-			instr.value = v
-			instr.operand = "assign"
-			instr.target = parts[2]
+			register[parts[2]] = uint16(v)
 		} else {
-			if parts[0] == "lx" {
-				fmt.Printf("found, len: %v\n", len(parts))
-			}
+			instr := instruction{}
+
 			switch len(parts) {
 			case 3:
 				instr.operand = "assign"
-				instr.inputA = parts[0]
-				instr.target = parts[2]
+				instr.wireA.name = parts[0]
+				instr.wireOut.name = parts[2]
+				instr.unary = true
 			case 4:
 				instr.operand = parts[0]
-				instr.inputA = parts[1]
-				instr.target = parts[3]
+				instr.wireA.name = parts[1]
+				instr.wireOut.name = parts[3]
+				instr.unary = true
 			case 5:
-				instr.inputA = parts[0]
+				instr.wireA.name = parts[0]
 				instr.operand = parts[1]
-				instr.inputB = parts[2]
-				instr.target = parts[4]
+				instr.wireB.name = parts[2]
+				instr.wireOut.name = parts[4]
+				instr.unary = false
 			}
+			booklet = append(booklet, instr)
 		}
-		booklet = append(booklet, instr)
+
+	}
+}
+
+func displayRegister() {
+	fmt.Println("\nregisters")
+	for name, v := range register {
+		fmt.Printf("%2v: %v\n", name, v)
 	}
 }
